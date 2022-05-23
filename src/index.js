83f677ae0ae87b0e344a64f0da385a722f8ebe15
Yu357 <forestgreen14689@gmail.com>
2022-05-23 22:46:21 +0900

    feat: TopページとNotFoundページへのルーティングを設定

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
 import Top from './screens/Top'
+import NotFound from './screens/NotFound'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Top />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Top/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>
 )
