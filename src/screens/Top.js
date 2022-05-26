01d8bff6d24a0522111a4cad7f24320c6dd63274
Yu357 <forestgreen14689@gmail.com>
2022-05-26 23:37:51 +0900

    feat: ドキュメントの読み取りに成功した

--- src/screens/Top.js
+++ src/screens/Top.js
@@ -1,6 +1,32 @@
 import styles from '../styles/top.module.css'
 
+import React, { useEffect, useState } from 'react';
+import { collection, query, onSnapshot } from "firebase/firestore";
+import db from '../utilities/Firebase';
+
 function Top() {
+
+  const [documents, setDocuments] = useState([])
+
+  useEffect(() => {
+    const q = query(collection(db, "threads"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push(doc);
+        console.log(`Thread id: ${doc.id}, title: ${doc.data().title}, createdAt: ${doc.data().createdAt}`);
+      });
+
+      setDocuments(docs);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+    
+  }, []);
+
+
   return (
     <main className={styles.main}>
       <div>
