34ab445de279e45bb01a6589a6711aa2deb1486b
Yu357 <forestgreen14689@gmail.com>
2022-05-27 00:22:07 +0900

    feat: cardにaタグを付与

--- src/components/ThreadCard.js
+++ src/components/ThreadCard.js
@@ -2,10 +2,12 @@ import styles from '../styles/threadCard.module.css'
 
 function ThreadCard(props) {
   return (
-    <div className={styles.card}>
-      <p className={styles.title}>{props.document.data().title}</p>
-      <p className={styles.comment}>You should use CSS Modules.</p>
-    </div>
+    <a href={`/${props.document.id}`} className={styles.cardAnchor}>
+      <div className={styles.card}>
+        <p className={styles.title}>{props.document.data().title}</p>
+        <p className={styles.comment}>You should use CSS Modules.</p>
+      </div>
+    </a>
   )
 }
 
