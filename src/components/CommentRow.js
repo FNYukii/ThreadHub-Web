c7ec05298d94917774abee844cbacc169935bbef
Yu357 <forestgreen14689@gmail.com>
2022-05-27 12:02:24 +0900

    feat: CommentRowをスタイリング

--- src/components/CommentRow.js
+++ src/components/CommentRow.js
@@ -1,8 +1,10 @@
+import styles from '../styles/commentRow.module.css'
+
 function CommentRow(props) {
   return (
-    <div>
-      <span>{props.dailyUserId}</span>
-      <span>2022-05-27</span>
+    <div className={styles.commentRow}>
+      <span className={styles.dailyUserId}>{props.dailyUserId}</span>
+      <span className={styles.createdAt}>2022-05-27</span>
       <p>{props.text}</p>
     </div>
   )
