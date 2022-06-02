ed6868cc8171f38bd9cc85d97d505445a453d85d
Yu357 <forestgreen14689@gmail.com>
2022-06-02 10:38:52 +0900

    feat: 入力状態設定

--- src/components/AddThreadModal.js
+++ src/components/AddThreadModal.js
@@ -1,8 +1,25 @@
 import styles from '../styles/addThreadModal.module.css'
 
 import { FaTimes } from 'react-icons/fa'
+import React, { useState } from 'react'
 
 function AddThreadModal(props) {
+
+  const [title, setTitle] = useState('')
+  const [detail, setDetail] = useState('')
+
+  const onInputTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const onInputDetail = (e) => {
+    setDetail(e.target.value)
+  }
+
+  const createThread = () => {
+    console.log(`title: ${title}, detail: ${detail}`)
+  }
+
   return (
     <div className={props.isOpenModal ? `${styles.modalShadow}` : `${styles.modalShadow} ${styles.invisible}`}>
       <div className={styles.modal}>
@@ -12,12 +29,12 @@ function AddThreadModal(props) {
         </button>
 
         <form>
-          <input placeholder="Thread title" required/>
-          <textarea placeholder='Thread detail' rows='5' required/>
+          <input placeholder="Thread title" required onChange={onInputTitle}/>
+          <textarea placeholder='Thread detail' rows='5' required onChange={onInputDetail}/>
         </form>
 
         <div className={styles.submitButtonContainer}>
-          <button>Create a thread</button>
+          <button onClick={createThread}>Create a thread</button>
         </div>
       
       </div>
