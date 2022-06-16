89897b11d518171fddec96bdb972d0211a1f4447
Yu357 <forestgreen14689@gmail.com>
2022-06-16 10:34:52 +0900

    feat: CommentRowにmenuButtonを配置

--- src/components/CommentRow.js
+++ src/components/CommentRow.js
@@ -1,13 +1,19 @@
 import styles from '../styles/commentRow.module.css'
 import dayjs from 'dayjs'
+import { FaEllipsisH } from 'react-icons/fa'
 
 function CommentRow(props) {
   return (
     <div className={styles.commentRow}>
       <div className={styles.nameBar}>
-        <span className={styles.displayName}>{props.displayName}</span>
-        <span className={styles.userId}>@{props.userId}</span>
-        <span className={styles.createdAt}>{dayjs(props.createdAt).format('YYYY/MM/DD HH:mm')}</span>
+        <div>
+          <span className={styles.displayName}>{props.displayName}</span>
+          <span className={styles.userId}>@{props.userId}</span>
+          <span className={styles.createdAt}>{dayjs(props.createdAt).format('YYYY/MM/DD HH:mm')}</span>
+        </div>
+        <button className={styles.menuButton}>
+          <FaEllipsisH/>
+        </button>
       </div>
       <p>{props.text}</p>
     </div>
