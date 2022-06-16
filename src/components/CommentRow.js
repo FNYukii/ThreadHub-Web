e17003525a7f76ef4d4483bdf501bf7375352b07
Yu357 <forestgreen14689@gmail.com>
2022-06-16 09:42:52 +0900

    feat: CommentRowにてuserIdがはみでないように設定

--- src/components/CommentRow.js
+++ src/components/CommentRow.js
@@ -3,9 +3,11 @@ import styles from '../styles/commentRow.module.css'
 function CommentRow(props) {
   return (
     <div className={styles.commentRow}>
-      <span className={styles.displayName}>{props.displayName}</span>
-      <span className={styles.userId}>@{props.userId}</span>
-      <span className={styles.createdAt}></span>
+      <div className={styles.nameBar}>
+        <span className={styles.displayName}>{props.displayName}</span>
+        <span className={styles.userId}>@{props.userId}</span>
+        <span className={styles.createdAt}></span>
+      </div>
       <p>{props.text}</p>
     </div>
   )
