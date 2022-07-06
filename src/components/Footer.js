efc0c556241fd3d83fc27199a26a4b14288a97ce
Yu357 <forestgreen14689@gmail.com>
2022-07-07 01:54:40 +0900

    feat: スタイリング

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,13 @@
+import styles from '../styles/footer.module.css'
+
 function Footer() {
   return (
-    <footer>
-      <a href="/">Thread Hub</a>
-      <p>誰でも利用できる匿名掲示板</p>
+    <footer className={styles.root}>
+      <div className={styles.largeContainer}>
+        <a href="/" className={styles.logo}>Thread Hub</a>
+        <p>誰でも利用できる匿名掲示板</p>
+        <p className={styles.copyright}>Copyright Yu357 2022.</p>
+      </div>
     </footer>
   )
 }
